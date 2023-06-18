c617764791b6fedb5f9598faa6b9f5d7d3998044
Sagnik Das <sagnikdas2305@gmail.com>
2023-06-18 03:45:49 +0000

    Gcloud YAML Config

--- main.py
+++ main.py
@@ -5,10 +5,24 @@ from fastapi import FastAPI, File, UploadFile
 from src.uploader import Uploader
 
 # Create a FastAPI app
-app = FastAPI()
+app = FastAPI(title="Casadona Warriors Data Endpoint",
+    description="This is a very cool API that does awesome stuff.",
+    version="1.0.0",
+    docs_url="/",
+    redoc_url="/cw-redoc",
+    openapi_url="/cw-openapi.json",
+    contact={
+        "name": "Sagnik Das",
+        "url": "www.github.com/sagnik-sudo",
+        "email": "sagnikdas2305@gmail.com"
+    },
+    license_info={
+        "name": "MIT License",
+        "url": "https://opensource.org/licenses/MIT"
+    })
 
 # Create an instance of the Uploader class with the bucket name
-uploader = Uploader("casadona-warriors")
+uploader = Uploader("casadona-warriors101")
 
 # Define a route for uploading a ZIP file
 @app.post("/upload_zip")
