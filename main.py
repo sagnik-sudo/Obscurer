9f1b7c7ebc5b936c77acd53adf6c60e4973033d7
Sagnik Das <sagnikdas2305@gmail.com>
2023-06-18 14:42:31 +0530

    Working Prototype - Uploader Script (Single file module)

--- main.py
+++ main.py
@@ -1,11 +1,7 @@
 # Importing the required modules
 from fastapi import FastAPI, File, UploadFile
-from zipfile import ZipFile
-import os
-import shutil
 import tempfile
-
-# Import the Uploader class from uploader.py
+import aiofiles
 from src.uploader import Uploader
 
 # Create a FastAPI app
@@ -16,41 +12,21 @@ app = FastAPI(title="Casadona Warriors Data Endpoint",
     redoc_url="/cw-redoc",
     )
 
-# Create an instance of the Uploader class
 uploader = Uploader()
-
-temp_folder = temp_folder = tempfile.gettempdir()
-os.makedirs(temp_folder, exist_ok=True)
 
 @app.get("/health")
 async def health():
     return {"Service Status":"Up and Running"}
 
-# Creating an endpoint to handle file upload
-@app.post("/upload")
-async def upload_file(file: UploadFile = File(...)):
-    # Saving the file to the temp folder
-    file_path = os.path.join(temp_folder, file.filename)
-    with open(file_path, "wb") as f:
-        shutil.copyfileobj(file.file, f)
-    # Checking if the file is a zip file
-    if file.filename.endswith(".zip"):
-        # Opening the zip file
-        with ZipFile(file_path, "r") as zip_file:
-            # Extracting all the files in the zip file to the temp folder
-            zip_file.extractall(temp_folder)
-            # Looping through the extracted files
-            for extracted_file in zip_file.namelist():
-                # Getting the extracted file path
-                extracted_file_path = os.path.join(temp_folder, extracted_file)
-                # Uploading the extracted file to Google Cloud Storage
-                uploader.upload_to_gcs(extracted_file_path, "your-bucket-name", extracted_file)
-                # Deleting the extracted file from the temp folder
-                os.remove(extracted_file_path)
-        # Deleting the zip file from the temp folder
-        os.remove(file_path)
-        # Returning a success message
-        return {"message": "Zip file uploaded and extracted successfully."}
-    else:
-        # Returning an error message
-        return {"error": "Please upload a zip file."}
+#Upload File
+@app.post("/upload-file/")
+async def create_upload_file(file: UploadFile):
+    file_name = file.filename
+    temp_dir = tempfile.gettempdir()
+    temp_f = f"{temp_dir}\{file_name}"
+    async with aiofiles.open(temp_f, 'wb') as out_file:
+        content = await file.read()  # async read
+        await out_file.write(content)  # async write
+    uploader.upload_to_bucket(file_name,temp_f,"casadonawarriors111234")
+    
+    return {"Upload Status":f"Saved Successfully - {file_name}"}
--- src/uploader.py
+++ src/uploader.py
@@ -1,7 +1,4 @@
-# Import the required packages
 from google.cloud import storage
-import zipfile
-import os
 
 # Define a class for uploading files to Google Cloud Storage
 class Uploader:
@@ -10,14 +7,13 @@ class Uploader:
         pass
 
     # Creating a function to upload a file to Google Cloud Storage
-    def upload_to_gcs(self,file_path, bucket_name, destination_blob_name):
-        # Initializing a storage client
-        storage_client = storage.Client()
-        # Getting the bucket object
-        bucket = storage_client.bucket(bucket_name)
-        # Creating a blob object
-        blob = bucket.blob(destination_blob_name)
-        # Uploading the file to the blob
-        blob.upload_from_filename(file_path)
-        # Printing the public URL of the blob
-        print(f"File {file_path} uploaded to {blob.public_url}.")
+    def upload_to_bucket(self,blob_name, path_to_file, bucket_name):
+        """ Upload data to a bucket"""
+        
+        # Explicitly use service account credentials by specifying the private key file.
+        storage_client = storage.Client.from_service_account_json('creds.json')
+        bucket = storage_client.get_bucket(bucket_name)
+        blob = bucket.blob(blob_name)
+        blob.upload_from_filename(path_to_file)
+        #returns a public url
+        return blob.public_url
