634d475f65e6fbb78f2991ccdf7d7b36d922fb2b
Sagnik Das <sagnikdas2305@gmail.com>
2023-06-18 13:07:28 +0530

    Checking fresh uploader script

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-# Import the required packages
+# Importing the required modules
 from fastapi import FastAPI, File, UploadFile
+from zipfile import ZipFile
+import os
+import shutil
+import tempfile
 
 # Import the Uploader class from uploader.py
 from src.uploader import Uploader
@@ -10,24 +14,43 @@ app = FastAPI(title="Casadona Warriors Data Endpoint",
     version="1.0.0",
     docs_url="/",
     redoc_url="/cw-redoc",
-    openapi_url="/cw-openapi.json",
-    contact={
-        "name": "Sagnik Das",
-        "url": "www.github.com/sagnik-sudo",
-        "email": "sagnikdas2305@gmail.com"
-    },
-    license_info={
-        "name": "MIT License",
-        "url": "https://opensource.org/licenses/MIT"
-    })
+    )
 
-# Create an instance of the Uploader class with the bucket name
-uploader = Uploader("casadona-warriors101")
+# Create an instance of the Uploader class
+uploader = Uploader()
 
-# Define a route for uploading a ZIP file
-@app.post("/upload_zip")
-async def upload_zip(zip_file: UploadFile = File(...)):
-    # Call the upload_zip method of the uploader instance with the zip_file as an argument
-    uploader.upload_zip(zip_file)
-    # Return a success message
-    return {"message": "ZIP file uploaded successfully"}
+temp_folder = temp_folder = tempfile.gettempdir()
+os.makedirs(temp_folder, exist_ok=True)
+
+@app.get("/health")
+async def health():
+    return {"Service Status":"Up and Running"}
+
+# Creating an endpoint to handle file upload
+@app.post("/upload")
+async def upload_file(file: UploadFile = File(...)):
+    # Saving the file to the temp folder
+    file_path = os.path.join(temp_folder, file.filename)
+    with open(file_path, "wb") as f:
+        shutil.copyfileobj(file.file, f)
+    # Checking if the file is a zip file
+    if file.filename.endswith(".zip"):
+        # Opening the zip file
+        with ZipFile(file_path, "r") as zip_file:
+            # Extracting all the files in the zip file to the temp folder
+            zip_file.extractall(temp_folder)
+            # Looping through the extracted files
+            for extracted_file in zip_file.namelist():
+                # Getting the extracted file path
+                extracted_file_path = os.path.join(temp_folder, extracted_file)
+                # Uploading the extracted file to Google Cloud Storage
+                uploader.upload_to_gcs(extracted_file_path, "your-bucket-name", extracted_file)
+                # Deleting the extracted file from the temp folder
+                os.remove(extracted_file_path)
+        # Deleting the zip file from the temp folder
+        os.remove(file_path)
+        # Returning a success message
+        return {"message": "Zip file uploaded and extracted successfully."}
+    else:
+        # Returning an error message
+        return {"error": "Please upload a zip file."}
--- src/uploader.py
+++ src/uploader.py
@@ -6,33 +6,18 @@ import os
 # Define a class for uploading files to Google Cloud Storage
 class Uploader:
     # Initialize the class with the bucket name and the storage client
-    def __init__(self, bucket_name):
-        self.bucket_name = bucket_name
-        self.storage_client = storage.Client()
+    def __init__(self):
+        pass
 
-    # Define a method for uploading a ZIP file and extracting its contents
-    def upload_zip(self, zip_file):
-        # Check if the file is a ZIP file
-        if zip_file.filename.endswith(".zip"):
-            # Save the ZIP file to a temporary location
-            temp_zip_file = f"temp/{zip_file.filename}"
-            with open(temp_zip_file, "wb") as f:
-                f.write(zip_file.file.read())
-            # Open the ZIP file and extract its contents
-            with zipfile.ZipFile(temp_zip_file, "r") as z:
-                z.extractall("temp")
-            # Delete the ZIP file from the temporary location
-            os.remove(temp_zip_file)
-            # Loop through the extracted files
-            for filename in os.listdir("temp"):
-                # Upload the file to Google Cloud Storage and get its location
-                blob = self.storage_client.bucket(self.bucket_name).blob(filename)
-                blob.upload_from_filename(f"temp/{filename}")
-                file_location = blob.public_url
-                # Delete the file from the temporary location
-                os.remove(f"temp/{filename}")
-                # Print the file location
-                print(f"Uploaded {filename} to {file_location}")
-        else:
-            # Print an error message if the file is not a ZIP file
-            print("Please upload a valid ZIP file")
+    # Creating a function to upload a file to Google Cloud Storage
+    def upload_to_gcs(self,file_path, bucket_name, destination_blob_name):
+        # Initializing a storage client
+        storage_client = storage.Client()
+        # Getting the bucket object
+        bucket = storage_client.bucket(bucket_name)
+        # Creating a blob object
+        blob = bucket.blob(destination_blob_name)
+        # Uploading the file to the blob
+        blob.upload_from_filename(file_path)
+        # Printing the public URL of the blob
+        print(f"File {file_path} uploaded to {blob.public_url}.")
