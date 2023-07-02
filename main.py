503cbd0cca3a5f94e20a7842abcd648b22784b28
Sagnik Das <sagnikdas2305@gmail.com>
2023-07-02 07:51:47 +0000

    Image to DLP App V1

--- main.py
+++ main.py
@@ -1,56 +1,107 @@
-# Importing the required modules
-from fastapi import FastAPI, File, UploadFile
-import tempfile
-import aiofiles
-from src.uploader import Uploader
-from typing import List
+from fastapi import FastAPI, UploadFile, File
+from google.cloud import storage, bigquery, vision_v1, dlp_v2
+from google.protobuf.json_format import MessageToDict
 import os
+import asyncio
+from typing import List
+from google.cloud import bigquery
+from google.cloud.bigquery.schema import SchemaField
 
-# Create a FastAPI app
-app = FastAPI(
-    title="Casadona Warriors Data Endpoint",
-    description="This project is a FastAPI application that takes image, pdf, or docx files as input and converts them to a readable text file, then applies PII (Personally Identifiable Information) deidentification and finally gives that as the output to the user.",
-    version="beta",
-    docs_url="/",
-    redoc_url="/cw-redoc",
-    contact={
-        "name": "Developer - Sagnik Das, Somdutta Paul, Tania Rana",
-        "email": "sagnik.das03@infosys.com",
-    },
-)
+app = FastAPI()
+project_id = "casacasa-390303"
+bucket_name = "sample1112345"
+gcs_client = storage.Client(project=project_id)
+bq_client = bigquery.Client(project=project_id)
+vision_client = vision_v1.ImageAnnotatorClient()
+dlp_client = dlp_v2.DlpServiceClient()
 
-BUCKET_NAME = "casa-bucket" # Modify bucket name here as per requirement
+table_ref = bq_client.dataset("metadata").table("files")
+table = bq_client.get_table(table_ref)
+schema = table.schema
 
-# Initializing Uploader Module
-uploader = Uploader()
 
-@app.get("/health",tags=["Service Status"], name="Application Health Check")
-async def health():
-    return {"Service Status":"Up and Running"}
-
-@app.post("/upload-file/",tags=["Upload Module"], name="Uploads single file to server")
-async def create_upload_file(file: UploadFile):
-    """Upload Single File"""
-    file_name = file.filename
-    temp_dir = tempfile.gettempdir()
-    temp_f = f"{temp_dir}\{file_name}"
-    async with aiofiles.open(temp_f, 'wb') as out_file:
-        content = await file.read()  # async read
-        await out_file.write(content)  # async write
-    uploader.upload_to_bucket(file_name,temp_f,BUCKET_NAME)
-    return {"Upload Status":f"Saved Successfully - {file_name}"}
-
-@app.post("/multi-upload/",tags=["Upload Module"], name="Uploads multiple files to server")
-async def multiupload(files: List[UploadFile] = File(...)):
-    """Upload Multiple Files"""
-    tempdir = tempfile.gettempdir()
+@app.post("/upload")
+async def upload_files(files: List[UploadFile] = File(...)):
     for file in files:
-        destination_file_path = os.path.join(tempdir, file.filename)
-        async with aiofiles.open(destination_file_path, 'wb') as out_file:
-        # async read file chunk
-            while content := await file.read(1024):
-                await out_file.write(content)  # async write file chunk
-    paths = [os.path.join(tempdir, file.filename) for file in files]
-    for i in range(len(paths)):
-        uploader.upload_to_bucket(files[i].filename,paths[i],BUCKET_NAME)
-    return {"Upload Status":{"Files Uploaded":files}}
+        # Store the uploaded file in Google Cloud Storage
+        blob = gcs_client.bucket(bucket_name).blob(file.filename)
+        blob.upload_from_file(file.file)
+
+        # Store metadata information in BigQuery
+        metadata = {
+            "filename": file.filename,
+            "content_type": file.content_type,
+            "size": file.file.seek(0, os.SEEK_END),
+        }
+        table = bq_client.dataset("metadata").table("files")
+        row = (metadata,)
+        bq_client.insert_rows(table, row, selected_fields=schema)  # Provide the table schema explicitly
+
+        # Process the uploaded file asynchronously
+        asyncio.create_task(process_file(blob))
+
+    return {"message": "Files uploaded and processing started."}
+
+
+async def process_file(blob):
+    # Process the file using Google Cloud Vision
+    response = vision_client.text_detection({
+        "source": {"image_uri": f"gs://{bucket_name}/{blob.name}"}
+    })
+
+    if response.text_annotations:
+        annotations = response.text_annotations[0].description
+    else:
+        annotations = ""
+
+    if annotations:
+        # Store the processed text in a different folder in the same bucket
+        processed_blob = gcs_client.bucket(bucket_name).blob(f"processed/{blob.name}.txt")
+        processed_blob.upload_from_string(annotations)
+
+        # Remove PII information using Google Cloud DLP
+        dlp_request = {
+            "parent": f"projects/{project_id}/locations/global",
+            "item": {"value": annotations},
+            "inspect_config": {
+                "info_types": [
+                    {"name": "PHONE_NUMBER"},
+                    {"name": "EMAIL_ADDRESS"},
+                    {"name": "PERSON_NAME"},
+                    {"name": "LOCATION"},
+                    {"name": "AGE"},
+                ]
+            },
+            "deidentify_config": {
+                "info_type_transformations": {
+                    "transformations": [
+                        {"primitive_transformation": {"replace_with_info_type_config": {}}}
+                    ]
+                }
+            },
+        }
+        dlp_response = dlp_client.deidentify_content(dlp_request)
+        deidentified_text = dlp_response.item.value
+
+        # Store the deidentified text in a different folder in the same bucket
+        deidentified_blob = gcs_client.bucket(bucket_name).blob(f"deidentified/{blob.name}.txt")
+        deidentified_blob.upload_from_string(deidentified_text)
+
+
+@app.get("/fetch/{name}")
+async def fetch_processed_text(name: str):
+    # Fetch processed text from Google Cloud Storage based on name
+    blobs = gcs_client.bucket(bucket_name).list_blobs(prefix="deidentified/")
+    text_files = [blob.name for blob in blobs if blob.name.endswith(".txt")]
+
+    matching_files = []
+    for file in text_files:
+        if name in file:
+            matching_files.append(file)
+
+    texts = []
+    for file in matching_files:
+        blob = gcs_client.bucket(bucket_name).blob(file)
+        texts.append(blob.download_as_text())
+
+    return {"texts": texts}
