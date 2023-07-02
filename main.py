9f92123887c88087a6321e4e5d1c9833ac2ae568
Sagnik Das <sagnikdas2305@gmail.com>
2023-07-02 13:30:39 +0000

    Working code for Obscurer

--- main.py
+++ main.py
@@ -9,6 +9,9 @@ from google.cloud import bigquery
 from google.cloud.bigquery.schema import SchemaField
 import datetime
 import uuid
+from google.api_core.client_options import ClientOptions
+from google.cloud import documentai
+
 
 app = FastAPI(
     title="Obscurer",
@@ -24,6 +27,8 @@ app = FastAPI(
 
 project_id = "casacasa-390303"
 bucket_name = "sample1112345"
+processor_id = "d13502f20685f48"
+location = "us"  # Replace with your processor's location
 
 gcs_client = storage.Client(project=project_id)
 bq_client = bigquery.Client(project=project_id)
@@ -79,28 +84,21 @@ async def upload_files(files: List[UploadFile] = File(...)):
 
 async def process_file(blob):
     logger.info(f"Processing file: {blob.name}")
+    # Read the file into memory
+    image_content = blob.download_as_bytes()
 
-    # Process the file using Google Cloud Vision
-    response = vision_client.text_detection({
-        "source": {"image_uri": f"gs://{bucket_name}/{blob.name}"}
-    })
-
-    if response.text_annotations:
-        annotations = response.text_annotations[0].description
-    else:
-        annotations = ""
-
-    if annotations:
-        # Store the processed text in a different folder in the same bucket
-        processed_blob = gcs_client.bucket(bucket_name).blob(f"processed/{blob.name}.txt")
-        processed_blob.upload_from_string(annotations)
-
-        logger.info(f"Stored processed text for file '{blob.name}' in Google Cloud Storage")
-
+        # Determine the file extension from the blob name
+    file_extension = blob.name.split(".")[-1].lower()
+    
+    # Skip Document AI processing for .txt files
+    if file_extension == "txt":
+        # Read the file content
+        text_content = blob.download_as_text()
+        
         # Remove PII information using Google Cloud DLP
         dlp_request = {
             "parent": f"projects/{project_id}/locations/global",
-            "item": {"value": annotations},
+            "item": {"value": text_content},
             "inspect_config": {
                 "info_types": [
                     {"name": "PHONE_NUMBER"},
@@ -126,6 +124,79 @@ async def process_file(blob):
         deidentified_blob.upload_from_string(deidentified_text)
 
         logger.info(f"Stored deidentified text for file '{blob.name}' in Google Cloud Storage")
+    else:
+        mime_type = ""
+        # Set the mime_type based on the file extension
+        if file_extension == "pdf":
+            mime_type = "application/pdf"
+        elif file_extension == "gif":
+            mime_type = "image/gif"
+        elif file_extension in ["tiff", "tif"]:
+            mime_type = "image/tiff"
+        elif file_extension in ["jpg", "jpeg"]:
+            mime_type = "image/jpeg"
+        elif file_extension == "png":
+            mime_type = "image/png"
+        elif file_extension == "bmp":
+            mime_type = "image/bmp"
+        elif file_extension == "webp":
+            mime_type = "image/webp"
+        else:
+            logger.error(f"Unsupported file type: {file_extension}")
+            return
+        
+        # Load Binary Data into Document AI RawDocument Object
+        raw_document = documentai.RawDocument(content=image_content, mime_type=mime_type)
+
+        # Configure the process request
+        docai_client = documentai.DocumentProcessorServiceClient(
+        client_options=ClientOptions(api_endpoint=f"{location}-documentai.googleapis.com")
+    )
+        RESOURCE_NAME = docai_client.processor_path(project_id, location, processor_id)
+        request = documentai.ProcessRequest(name=RESOURCE_NAME, raw_document=raw_document)
+
+        # Use the Document AI client to process the document
+        result = docai_client.process_document(request=request)
+
+        document_object = result.document
+        annotations = document_object.text
+
+        if annotations:
+            # Store the processed text in a different folder in the same bucket
+            processed_blob = gcs_client.bucket(bucket_name).blob(f"processed/{blob.name}.txt")
+            processed_blob.upload_from_string(annotations)
+
+            logger.info(f"Stored processed text for file '{blob.name}' in Google Cloud Storage")
+
+            # Remove PII information using Google Cloud DLP
+            dlp_request = {
+                "parent": f"projects/{project_id}/locations/global",
+                "item": {"value": annotations},
+                "inspect_config": {
+                    "info_types": [
+                        {"name": "PHONE_NUMBER"},
+                        {"name": "EMAIL_ADDRESS"},
+                        {"name": "PERSON_NAME"},
+                        {"name": "LOCATION"},
+                        {"name": "AGE"},
+                    ]
+                },
+                "deidentify_config": {
+                    "info_type_transformations": {
+                        "transformations": [
+                            {"primitive_transformation": {"replace_with_info_type_config": {}}}
+                        ]
+                    }
+                },
+            }
+            dlp_response = dlp_client.deidentify_content(dlp_request)
+            deidentified_text = dlp_response.item.value
+
+            # Store the deidentified text in a different folder in the same bucket
+            deidentified_blob = gcs_client.bucket(bucket_name).blob(f"deidentified/{blob.name}.txt")
+            deidentified_blob.upload_from_string(deidentified_text)
+
+            logger.info(f"Stored deidentified text for file '{blob.name}' in Google Cloud Storage")
 
 
 @app.get("/fetch/{name}")
